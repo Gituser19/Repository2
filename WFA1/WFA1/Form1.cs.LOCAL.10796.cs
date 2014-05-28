4e855429654a61c54261ac64b0ecae22e936b27b
Ramya <ramya.nandakumar@non.agilent.com>
2014-05-28 09:54:46 -0700

    Merge branch 'master' of https://github.com/Gituser19/Repository2
    Conflicts:
    	WFA1/WFA1/Form1.cs

---
+++ WFA1/WFA1/Form1.cs.LOCAL.10796.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+//Change from User2
+//Change from User1 now
+//Again User2 makes a change!
+//Now User1 makes a change on top of it!!!
+//continuation...User2 made a update today
+//User2-makes a update which User1 doesn't know
+        }
+    }
+}
