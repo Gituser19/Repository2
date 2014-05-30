f1a4c097dcce9569b4a69f8df0a3d268c15448a5
Ramya <ramya.nandakumar@non.agilent.com>
2014-05-29 17:07:22 -0700

    Renamed my module

---
+++ R1-Module1/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+//This is my individual module.
+//1-made changes to my module
+//2-made my second change to my module
+//3-made third change to my module
+//4-made fourth change to my module
+        }
+    }
+}
